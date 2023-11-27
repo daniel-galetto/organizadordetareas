4c324249cdd31bd843e99d06836c9242139f37bd
daniel-galetto <daniel.galetto.arg@gmail.com>
2023-11-26 23:54:19 -0300

    fix image carousel

--- src/components/CarouselHome/CarouselHome.tsx
+++ src/components/CarouselHome/CarouselHome.tsx
@@ -8,7 +8,7 @@ const CarouselHome = () => {
                     <img
                         className="d-block w-100"
                         style={{ maxHeight: "500px", objectFit: "cover",}}
-                        src="src/assets/images/image3.jpg"
+                        src="https://img.freepik.com/foto-gratis/mano-femenina-escribiendo-teclado-portatil_1150-15742.jpg?w=740&t=st=1701053397~exp=1701053997~hmac=402710090c00120f57a013e74490d302fdf8ce7131b4a701fd23cb9c8fc75183"
                         alt='organizdor de tareas'
                     />
                     <Carousel.Caption className='bg-black'>
@@ -21,7 +21,7 @@ const CarouselHome = () => {
                     <img
                         className="d-block w-100"
                         style={{ maxHeight: "500px", objectFit: "cover" }}
-                        src="src/assets/images/image.jpg"
+                        src="https://github.com/daniel-galetto/organizadordetareas/blob/945ce3193646c7418dbb7ad9697d651ebc313e0d/src/assets/images/image.jpg"
                         alt='organizdor de tareas'
                     />
                     <Carousel.Caption className='bg-black'>
@@ -34,7 +34,7 @@ const CarouselHome = () => {
                     <img
                         className="d-block w-100"
                         style={{ maxHeight: "500px", objectFit: "cover" }}
-                        src="src/assets/images/image1.jpg"
+                        src="/home/daniel/developer/utnreact/trabajoIntegrador/organizadorTareas/organizadortareas/src/assets/images/image1.jpg"
                         alt='organizdor de tareas'
                     />
                     <Carousel.Caption className='bg-black'>
